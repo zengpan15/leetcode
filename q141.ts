2543e6f58f750fec43dffa514aa93e33f697ad70
Panpan Zeng <3198612440@qq.com>
2022-03-16 23:54:26 -0700

    leetcode question 141:huanxinglianbiao

---
+++ q141.ts
@@ -0,0 +1,17 @@
+function hasCycle(head: ListNode | null): boolean {
+    if (!head || !head.next) {
+        return false;
+    }
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
+    }
+
+    return false;
+    
+};
